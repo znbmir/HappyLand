19d2e41da1da7db6f29bbd6422e3ee968145f022
Zeynab <znb.mir@gmail.com>
2019-05-29 12:51:39 +0430

    May 29 2019

---
+++ HappyLand/Assets/Scripts/Notes/ObstacleOffScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleOffScreen : MonoBehaviour {
+
+  void OnTriggerEnter2D(Collider2D target) {
+		if (target.tag == "Collector") {
+			gameObject.SetActive(false);
+      DestroyOnTouch.passTouchActivation = false;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
